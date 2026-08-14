bf6be5554c818f6154e3b610b252c6752b1b1968
yusufuca <yusemuca@gmail.com>
2026-08-15 02:23:16 +0300

    Basic Player Movement and Anims

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     CharacterController charController;
+    Animator animator;
     Vector3 targetPos;
     public float Speed = 5f;
     public Vector3 playerVelocity;
     public bool isGrounded;
     public float gravityValue = -9.8f;
     public float jumpHeight = 2f;
+
+    Vector3 lastPos;
     void Start()
     {
         charController = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
         targetPos = transform.position;
     }
 
@@ -19,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         isGrounded = charController.isGrounded;
+        lastPos = transform.position;
         if (isGrounded)
         {
             if(playerVelocity.y < -2)
@@ -34,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
         playerVelocity.y += gravityValue * Time.deltaTime;
         MoveToPoisiton();
         MovementKeys();
+        AnimateChar();
     }
     private void MoveToPoisiton()
     {
@@ -50,6 +56,11 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector3 direction = (targetPos - transform.position).normalized;
             finalDestination = direction * Speed;
+            Vector3 lookDirection = new Vector3(direction.x, 0, direction.z);
+            if (lookDirection != Vector3.zero)
+            {
+                transform.forward = lookDirection;
+            }
         }
         
 
@@ -61,7 +72,28 @@ public class PlayerMovement : MonoBehaviour
         float mvFB = Input.GetAxis("Vertical") * Speed;
         Vector3 movement = new Vector3(mvRL, playerVelocity.y, mvFB);
         if(mvRL != 0 || mvFB != 0) 
-        { targetPos = transform.position; }
+        {
+            targetPos = transform.position;
+            Vector3 lookDirection = new Vector3(mvRL, 0, mvFB);
+            transform.forward = lookDirection;
+        }
+      
         charController.Move(movement * Time.deltaTime);
+        
+    }
+    private void DetectFacing()
+    {
+        
+    }
+    private void AnimateChar()
+    {
+        if (lastPos != transform.position) 
+        {
+            animator.SetBool("isMoving" , true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
     }
 }
